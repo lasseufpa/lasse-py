08920e860c270dc291e08cd4c85ac4658546334a
1176325 <1176325@gitlab.lasse.ufpa.br>
2023-01-26 09:28:04 -0300

    Force compliance with PEP

--- lasse/io/matlab_tofrom_python.py
+++ lasse/io/matlab_tofrom_python.py
@@ -1,10 +1,7 @@
-import numpy as np
-import scipy.io as spio
-import h5py
-'''
+"""
 Code to interface Python and Matlab. It allows to read arrays (one
 needs to know the name of the array):
-Matlab to Python: read .mat (version 6) files into Python 
+Matlab to Python: read .mat (version 6) files into Python
 Python to Matlab: write HDF5 files to be read from Matlab with our companion function
 readArrayFromHDF5.m.
 
@@ -13,14 +10,19 @@ Example:
     save -v6 'test.mat' arrayName1 arrayName2 arrayName3
     or
     save(thisFileName,'myArray','-v6')
-'''
+"""
+
+import h5py
+import numpy as np
+import scipy.io as spio
+
 
 def convert_npz_to_hdf5(npzFileName):
-    '''
+    """
     Convert npz into hdf5. Note hdf5 does not support compression and files can be much larger
     than a compressed npz. But the hdf5 can be conveniently read by Matlab.
     See: http://christopherlovell.co.uk/blog/2016/04/27/h5py-intro.html
-    %To read hdf5 file in Matlab as follows:    
+    %To read hdf5 file in Matlab as follows:
     %https://stackoverflow.com/questions/21624653/python-created-hdf5-dataset-transposed-in-matlab
     %do not forget the / as prefix onf the second argument below:
     allEpisodeData=h5read(fileName,'/allEpisodeData');
@@ -28,25 +30,25 @@ def convert_npz_to_hdf5(npzFileName):
     allEpisodeData = permute(allEpisodeData,ndims(allEpisodeData):-1:1);
     :param npzFileName:
     :return:
-    '''
+    """
     npz_cache_file = np.load(npzFileName)
     allKeys = npz_cache_file.keys()
-    numKeys = len(allKeys)
-    outputHDF5FilaName = npzFileName.replace('.npz','.hdf5')
-    h5pyFile = h5py.File(outputHDF5FilaName, 'w')
+    outputHDF5FilaName = npzFileName.replace(".npz", ".hdf5")
+    h5pyFile = h5py.File(outputHDF5FilaName, "w")
     for thisKey in allKeys:
-        npArray = npz_cache_file[thisKey] #get array with name thisKey
-        #Matched ok with result of test_convert_npz_to_hdf5.m
+        npArray = npz_cache_file[thisKey]  # get array with name thisKey
+        # Matched ok with result of test_convert_npz_to_hdf5.m
         # squeeze(obstacles_matrix_array(10,4,196,570,1:15))'
-        #that was: 0   1   1   1   1   1   1   1   1   1   0   0   0   0   0
-        #print(npArray[9,3,195,569,0:15])
-        #output the same result
-        h5pyFile[thisKey] = npArray #store in hdf5
+        # that was: 0   1   1   1   1   1   1   1   1   1   0   0   0   0   0
+        # print(npArray[9,3,195,569,0:15])
+        # output the same result
+        h5pyFile[thisKey] = npArray  # store in hdf5
     h5pyFile.close()
-    print('==> Wrote file ', outputHDF5FilaName, ' with keys: ', allKeys)
+    print("==> Wrote file ", outputHDF5FilaName, " with keys: ", allKeys)
+
 
 def read_matrix_from_file(fileName, numRows, numColumns):
-    '''
+    """
     Read in Python a real-valued matrix written in Matlab as binary file with doubles (64 bits).
     We deal here with transposing the matrix given that Matlab organizes the numbers
     ordering along columns while Python, C, etc. go along the rows.
@@ -69,24 +71,25 @@ def read_matrix_from_file(fileName, numRows, numColumns):
     :param numRows: number of rows
     :param numColumns: number of columns
     :return: matrix of dimension numRows x numColumns
-    '''
-    matrix = np.fromfile(fileName,dtype=np.double)
-    #there is a small header: first two values are the matrix dimension
-    if matrix[0] != numRows: #check consistency
-        print(matrix[0], ' != ', numRows)
+    """
+    matrix = np.fromfile(fileName, dtype=np.double)
+    # there is a small header: first two values are the matrix dimension
+    if matrix[0] != numRows:  # check consistency
+        print(matrix[0], " != ", numRows)
         exit(-1)
-    if matrix[1] != numColumns: #check consistency
-        print(matrix[1], ' != ', numColumns)
+    if matrix[1] != numColumns:  # check consistency
+        print(matrix[1], " != ", numColumns)
         exit(-1)
-    matrix = matrix[2:] #skip the header (two first values)
-    #recall that it was written along columns in Matlab, so will need to transpose
-    #in order to adjust to the way Python reads it in
-    matrix = np.reshape(matrix,(numColumns, numRows)) #read with "transposed" shape
-    matrix = matrix.T #now effectively transpose elements
+    matrix = matrix[2:]  # skip the header (two first values)
+    # recall that it was written along columns in Matlab, so will need to transpose
+    # in order to adjust to the way Python reads it in
+    matrix = np.reshape(matrix, (numColumns, numRows))  # read with "transposed" shape
+    matrix = matrix.T  # now effectively transpose elements
     return matrix
 
+
 def read_matlab_array_from_mat(fileName, arrayName):
-    '''
+    """
     Read an array saved in Matlab with something like:
     save -v6 'test.mat' arrayName1 arrayName2 arrayName3
     or
@@ -95,15 +98,16 @@ def read_matlab_array_from_mat(fileName, arrayName):
     :param fileName: file name (test.mat in this example)
     :param arrayName: name of array of interest
     :return: numpy array
-    '''
-    arrays=spio.loadmat(fileName)
-    x=arrays[arrayName]
-    #return np.transpose(x) #scipy already transposes. No need to:
-    #https://stackoverflow.com/questions/21624653/python-created-hdf5-dataset-transposed-in-matlab
+    """
+    arrays = spio.loadmat(fileName)
+    x = arrays[arrayName]
+    # return np.transpose(x) #scipy already transposes. No need to:
+    # https://stackoverflow.com/questions/21624653/python-created-hdf5-dataset-transposed-in-matlab
     return x
 
+
 def read_several_matlab_arrays_from_mat(fileName, arrayNames):
-    '''
+    """
     Read an array saved in Matlab with something like:
     save -v6 'test.mat' arrayName1 arrayName2 arrayName3
     or
@@ -112,53 +116,59 @@ def read_several_matlab_arrays_from_mat(fileName, arrayNames):
     :param fileName: file name (test.mat in this example)
     :param arrayNames: list or tuple with name of arrays of interest
     :return: list with numpy arrays
-    '''
-    arrays=spio.loadmat(fileName)
+    """
+    arrays = spio.loadmat(fileName)
     x = list()
     for i in range(len(arrayNames)):
         x.append(arrays[arrayNames[i]])
-        #return np.transpose(x) #scipy already transposes. No need to:
-        #https://stackoverflow.com/questions/21624653/python-created-hdf5-dataset-transposed-in-matlab
+        # return np.transpose(x) #scipy already transposes. No need to:
+        # https://stackoverflow.com/questions/21624653/python-created-hdf5-dataset-transposed-in-matlab
     return x
 
+
 def test_convert_npz_to_hdf5():
-    fileName = 'D:/gits/lasse/software/5gm-pcd-nn/3dobstacles/aug11norotation/obstacles_e_1.npz'
+    fileName = "D:/gits/lasse/software/5gm-pcd-nn/3dobstacles/aug11norotation/obstacles_e_1.npz"
     print("Reading dataset...", fileName)
     convert_npz_to_hdf5(fileName)
+
 
 def test_read_matlab_array_from_mat():
 
     if False:
-        #some example of complex-valued 3-d array called H_all in a mat file:
-        fileName = 'D:/ak/Works/2018-massive-mimo/GampMatlab/trunk/code/examples/ProbitSE/test3.mat'
-        arrayName = 'H_all'
-        x=read_matlab_array_from_mat(fileName, arrayName)
-        print(x[50,60,70])
+        # some example of complex-valued 3-d array called H_all in a mat file:
+        fileName = "D:/ak/Works/2018-massive-mimo/GampMatlab/trunk/code/examples/ProbitSE/test3.mat"
+        arrayName = "H_all"
+        x = read_matlab_array_from_mat(fileName, arrayName)
+        print(x[50, 60, 70])
     else:
         if True:
-            #this array is complex and has shape (140, 7, 4, 2, 18, 50)
-            fileName = 'D:/gits/lasse/software/mimo-matlab/clusteredChannels/802_16_outdoor/mainFolder/allChannelsEpisode1.mat'
-            arrayName = 'allHs'
-            x=read_matlab_array_from_mat(fileName, arrayName)
+            # this array is complex and has shape (140, 7, 4, 2, 18, 50)
+            fileName = "D:/gits/lasse/software/mimo-matlab/clusteredChannels/802_16_outdoor/mainFolder/allChannelsEpisode1.mat"
+            arrayName = "allHs"
+            x = read_matlab_array_from_mat(fileName, arrayName)
             print(x.shape)
-            print(x[50,6,3,1,3,40]) #and compare with allHs(50+1,6+1,3+1,1+1,3+1,40+1) in Matlab
+            print(
+                x[50, 6, 3, 1, 3, 40]
+            )  # and compare with allHs(50+1,6+1,3+1,1+1,3+1,40+1) in Matlab
         else:
-            #another example saved with
-            #save(fileName,'ak_receivedSignal_y', 'ak_transmittedSignal_x', 'ak_channel', 'ak_noise','-v6');
-            fileName = 'gamp_simulation.mat'
-            arrayName = 'ak_receivedSignal_y'
-            x=read_matlab_array_from_mat(fileName, arrayName)
-            #Matlab: ak_receivedSignal_y(10,5,20)
-            print(x[9,4,19])
+            # another example saved with
+            # save(fileName,'ak_receivedSignal_y', 'ak_transmittedSignal_x', 'ak_channel', 'ak_noise','-v6');
+            fileName = "gamp_simulation.mat"
+            arrayName = "ak_receivedSignal_y"
+            x = read_matlab_array_from_mat(fileName, arrayName)
+            # Matlab: ak_receivedSignal_y(10,5,20)
+            print(x[9, 4, 19])
+
 
 def test_read_matlab_array_from_mat2():
-    fileName = 'D:/gits/lasse/software/2019-aldebaro-ita-fwrl/outputs/channels_f3_b12tauc190taup30e_10.mat'
-    arrayName = 'H'
+    fileName = "D:/gits/lasse/software/2019-aldebaro-ita-fwrl/outputs/channels_f3_b12tauc190taup30e_10.mat"
+    arrayName = "H"
     H = read_matlab_array_from_mat(fileName, arrayName)
     print(H.shape)
-    print(H[1,2,3,1])
+    print(H[1, 2, 3, 1])
 
-if __name__ == '__main__':
-    #test_read_matlab_array_from_mat()
-    #test_convert_npz_to_hdf5()
+
+if __name__ == "__main__":
+    # test_read_matlab_array_from_mat()
+    # test_convert_npz_to_hdf5()
     test_read_matlab_array_from_mat2()
