44b271e70836c659d14ba05c8e12f570ccfaa1a4
1176325 <1176325@gitlab.lasse.ufpa.br>
2023-01-26 10:08:02 -0300

    PEP compliance

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
+# will delete
+from setuptools import find_packages, setup
 
-from setuptools import setup, find_packages
-
-setup(name='lasse-py',
-      version='0.0.1',
-      description='Python code for DSP, ML and telecom',
-      author='LASSE',
-      author_email='lasse',
-      packages=find_packages(exclude=['tests']),
-      url='https://github.com/lasseufpa/lasse-py',
-      install_requires=['numpy(>=1.14)'])
+setup(
+    name="lasse-py",
+    version="0.0.1",
+    description="Python code for DSP, ML and telecom",
+    author="LASSE",
+    author_email="lasse",
+    packages=find_packages(exclude=["tests"]),
+    url="https://github.com/lasseufpa/lasse-py",
+    install_requires=["numpy(>=1.14)"],
+)
